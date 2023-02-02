ef1e27d52d68596ddd2314a2c899f351df690d98
Bastien Daniel <bastien.daniel@rawfish.com>
2023-02-02 10:16:39 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
 import { Component } from '@angular/core';
+import { StorageService } from './services/storage/storage.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'angular-local-storage';
+  person = { name: '', country: '' };
+  storageName: string | undefined;
+  storageObject: Object = {};
+
+  constructor(private storageService: StorageService) {}
+
+  setStorage() {
+    this.storageService.setItem('name', this.person.name);
+    this.storageService.setItem('country', this.person.country);
+    this.storageService.setItem('person', {
+      name: this.person.name,
+      country: this.person.country,
+    });
+    alert('Set storage variable and object successfull');
+  }
+
+  getStorage() {
+    this.storageName = this.storageService.getItem('name');
+    this.storageObject = this.storageService.getItem('person');
+  }
+
+  removeItemStorage(key: string) {
+    this.storageService.removeItem(key);
+  }
+
+  clearAllStorage() {
+    this.storageService.clear();
+    this.storageName = '';
+    this.storageObject = {};
+  }
 }
